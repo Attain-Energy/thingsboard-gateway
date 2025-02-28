f64af6b4266b1c9116b678a514869b45df1b9810
Boro <57097132+Boris-Karamanolev@users.noreply.github.com>
2025-02-28 17:50:46 +0200

    init

---
+++ thingsboard_gateway/connectors/haystack/haystack_scram_connector.py
@@ -0,0 +1,68 @@
+import logging
+from thingsboard_gateway.connectors.connector import Connector
+import scramp
+
+logger = logging.getLogger("HaystackScramConnector")
+
+class HaystackScramConnector(Connector):
+    def __init__(self, gateway, config):
+        super().__init__(gateway, config)
+        self.config = config
+        self.username = config.get("username")
+        self.password = config.get("password")
+        self.scram_mechanism = config.get("scram_mechanism", "SCRAM-SHA-256")
+        self.authenticated = False
+        self.scram_client = scramp.ScramClient(self.scram_mechanism)
+
+    def start(self):
+        logger.info("Starting Haystack SCRAM Connector...")
+        self._authenticate()
+
+    def stop(self):
+        logger.info("Stopping Haystack SCRAM Connector...")
+
+    def _authenticate(self):
+        try:
+            # Generate client first message.
+            client_first = self.scram_client.get_client_first_message(self.username)
+            logger.debug("Client first message: %s", client_first)
+
+            # TODO: Send client_first to your server
+            # self.send_message(client_first)
+
+            # TODO: Receive server's challenge message.
+            server_first = self.receive_server_first()  # Implement this based on your transport
+            logger.debug("Received server first message: %s", server_first)
+
+            # Process server challenge and generate final message.
+            client_final = self.scram_client.process_server_first_message(server_first, self.password)
+            logger.debug("Client final message: %s", client_final)
+
+            # TODO: Send client_final to the server
+            # self.send_message(client_final)
+
+            # TODO: Receive final server message.
+            server_final = self.receive_server_final()  # Implement this based on your transport
+            logger.debug("Received server final message: %s", server_final)
+
+            # Complete the authentication process.
+            self.scram_client.process_server_final_message(server_final)
+            self.authenticated = True
+            logger.info("SCRAM authentication successful.")
+        except Exception as e:
+            logger.error("Authentication error: %s", e)
+            self.authenticated = False
+
+    # --- Placeholder methods: Implement these for actual communication ---
+    def receive_server_first(self):
+        raise NotImplementedError("receive_server_first not implemented")
+
+    def receive_server_final(self):
+        raise NotImplementedError("receive_server_final not implemented")
+
+    # --- Optional callbacks ---
+    def on_attributes_update(self, content):
+        pass
+
+    def server_side_rpc_handler(self, content):
+        pass
